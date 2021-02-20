4d3523fbcafd79e15c5e62701a0174927143c68f
Try Wijono <3wijono@gmail.com>
2021-02-20 16:23:06 +0800

    main input field

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useFetch } from "./components/util";
+import Calculator from "./components/Calculator";
 
 function App() {
+  const [data, dataLoading, dataHasError] = useFetch("models/MOCK_DATA.json");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {dataLoading ? (
+        <div>Loading...</div>
+      ) : dataHasError ? (
+        <div>Error Load Occured</div>
+      ) : (
+        <div>
+          <Calculator data={data} />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Calculator.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+const Calculator = ({ data }) => {
+  const [housingType, setHousingType] = useState();
+  const [principal, setPrincipal] = useState();
+  const [duration, setDuration] = useState();
+
+  const buttonClick = () => {
+    const principalVal = document.querySelector("#principal").value;
+    const durationVal = document.querySelector("#duration").value;
+    setHousingType(document.querySelector("#housingType").value);
+    if (!principalVal && !durationVal) {
+      document.querySelectorAll(".field")[1].classList.add("error");
+      document.querySelectorAll(".field")[2].classList.add("error");
+      return;
+    }
+    if (!principalVal) {
+      document.querySelectorAll(".field")[1].classList.add("error");
+      return;
+    }
+    if (!durationVal) {
+      document.querySelectorAll(".field")[2].classList.add("error");
+      return;
+    }
+    document.querySelectorAll(".field")[1].classList.remove("error");
+    document.querySelectorAll(".field")[2].classList.remove("error");
+    setPrincipal(principalVal);
+    setDuration(durationVal);
+  };
+
+  return (
+    <div className="ui stackable grid">
+      <div className="one wide column"></div>
+      <div className="three wide column">
+        <div className="ui form">
+          <div className="field">
+            <label>Type of Housing</label>
+            <select className="ui fluid dropdown" id="housingType">
+              <option value="1">HDB</option>
+              <option value="2">Private Residence</option>
+              <option value="3">Commercial Property</option>
+            </select>
+          </div>
+          <div className="field">
+            <label>Principal in SG$</label>
+            <input
+              type="number"
+              name="principal"
+              step="100"
+              placeholder="SGD XXX,XXX"
+              id="principal"
+            ></input>
+          </div>
+          <div className="field">
+            <label>Loan Duration in Years</label>
+            <input
+              type="number"
+              name="duration"
+              placeholder="number of years"
+              id="duration"
+            ></input>
+          </div>
+          <button className="ui button" onClick={buttonClick}>
+            Submit
+          </button>
+        </div>
+      </div>
+
+      <div className="eleven wide column"></div>
+    </div>
+  );
+};
+
+export default Calculator;
